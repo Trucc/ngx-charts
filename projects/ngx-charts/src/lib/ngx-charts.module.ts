76bb5026bae76ae014d3e2677bc189446fb3bc9b
Nicolas FRBEZAR <nicolas.frbezar@actemium.com>
2018-07-26 09:43:18 +0200

    Generate initial library

---
+++ projects/ngx-charts/src/lib/ngx-charts.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxChartsComponent } from './ngx-charts.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [NgxChartsComponent],
+  exports: [NgxChartsComponent]
+})
+export class NgxChartsModule { }
